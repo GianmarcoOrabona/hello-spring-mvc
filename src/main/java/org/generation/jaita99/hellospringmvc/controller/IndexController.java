d7a2c4350dc8637efe4346c18d859e0c38bd8a29
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-03 14:51:21 +0100

    final commit - add bonus

--- src/main/java/org/generation/jaita99/hellospringmvc/controller/IndexController.java
+++ src/main/java/org/generation/jaita99/hellospringmvc/controller/IndexController.java
@@ -4,16 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Random;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+
+    // Metodo che restituisce un numero random da un lancio di un dado
     @GetMapping
-    public String index(Model model) {
+    public String index(@RequestParam int dice, Model model) {
+
         Random random = new Random();
-        int randomNumber = random.nextInt(1, 7);
+        int randomNumber = random.nextInt(1, (dice + 1));
+
+        // Aggiungo l'attributo
         model.addAttribute("number", randomNumber);
         return "home";
     }
