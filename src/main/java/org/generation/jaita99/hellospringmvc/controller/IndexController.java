bbfcf3d439ca0223471d711c31edacad079cc808
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-03 14:44:16 +0100

    final commit

--- src/main/java/org/generation/jaita99/hellospringmvc/controller/IndexController.java
+++ src/main/java/org/generation/jaita99/hellospringmvc/controller/IndexController.java
@@ -1,9 +1,20 @@
 package org.generation.jaita99.hellospringmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Random;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    @GetMapping
+    public String index(Model model) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(1, 7);
+        model.addAttribute("number", randomNumber);
+        return "home";
+    }
 }
