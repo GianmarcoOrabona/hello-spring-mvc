cc6d508bd434003338ab8ead261b5000fc625152
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-03 14:34:28 +0100

    Initial commit

---
+++ src/main/java/org/generation/jaita99/hellospringmvc/controller/IndexController.java
@@ -0,0 +1,9 @@
+package org.generation.jaita99.hellospringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+}
---
+++ src/test/java/org/generation/jaita99/hellospringmvc/HelloSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.jaita99.hellospringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
